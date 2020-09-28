0710c524374683e6fffce23a17f1b0f39a0df285
micaelgallego <micael.gallego@gmail.com>
2020-09-28 13:33:12 +0200

    Initial commit

---
+++ 7-users-reactive/src/main/java/es/codeurjc/users/DatabaseInitializer.java
@@ -0,0 +1,49 @@
+package es.codeurjc.users;
+
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+@Component
+@DependsOn({"initializer"})
+public class DatabaseInitializer {
+
+    private UserRepository userRepository;
+
+    DatabaseInitializer(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @PostConstruct
+    public void initDatabase() {
+
+        Flux<User> users = Flux.just(
+            new User("Juan", "López"),
+            new User("Pepe", "Reina"),
+            new User("Juan", "García"));
+
+        users
+            .flatMap(this.userRepository::save)
+            .blockLast();
+    }
+
+//    Los métodos @PostConstruct no pueden ser asíncronos. Hay que forzar la suscripción o bloquear hasta finalización
+//    @PostConstruct
+//    public Mono<Void> initDatabase() {
+//
+//        Flux<User> users = Flux.just(
+//            new User("Juan", "López"),
+//            new User("Pepe", "Reina"),
+//            new User("Juan", "García"));
+//
+//        return users
+//            .flatMap(this.userRepository::save)
+//            .then(Mono.empty());
+//    }
+}
---
+++ 7-users-reactive/src/main/java/es/codeurjc/users/UserController.java
@@ -0,0 +1,59 @@
+package es.codeurjc.users;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.stream.Collectors.*;
+
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/users/")
+public class UserController {
+
+    private UserService userService;
+
+    UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<UserDTO> createUser(@RequestBody Mono<UserDTO> user) {
+        return user
+            .map(this::toUser)
+            .flatMap(userService::createUser)
+            .map(this::toUserDTO);
+    }
+
+    @GetMapping("/")
+    public Flux<UserDTO> getUsers(@RequestParam(required = false) Optional<String> firstName) {
+        return userService.getUsers(firstName)
+            .map(this::toUserDTO);
+    }
+
+    @GetMapping("/{id}")
+    public Mono<UserDTO> getUser(@PathVariable Long id) {
+        return userService.getUser(id)
+            .map(this::toUserDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<UserDTO> deleteUser(@PathVariable Long id) {
+        return userService.deleteUser(id).map(this::toUserDTO);
+    }
+
+    private User toUser(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getFirstName(), userDTO.getLastName());
+    }
+
+    private UserDTO toUserDTO(User user) {
+        return new UserDTO(user.getId(), user.getFirstName(), user.getLastName());
+    }
+
+}
